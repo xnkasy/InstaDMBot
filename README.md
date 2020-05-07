5c41137599c2695426605b26680c4dbbf53a20b0
xnkasy <xnkasypriyanshu@gmail.com>
2020-05-07 21:01:15 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# What's-App-Automation
+
+[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)                  [![Python 3.6](https://img.shields.io/badge/python-3.6-blue.svg)](https://www.python.org/downloads/release/python-360/)          [![PyPI license](https://img.shields.io/pypi/l/ansicolortags.svg)](https://pypi.python.org/pypi/ansicolortags/)
+
+
+It is What's-App-Automation using python &amp; selenium to send messages and images/videos
+
+
+
+# VIDEO:
+
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/lWa6hyqT_QI/0.jpg)](https://www.youtube.com/watch?v=lWa6hyqT_QI)
+
+## Dependencies:
+
+Selenium
+
+```
+pip install selenium
+```
+
+
+Also Download the Chrome Drivers from here according to your chrome version :- [click here](https://chromedriver.chromium.org/downloads)
+
+
+## Owner:
+- [Arbazkhan4712](https://github.com/Arbazkhan4712/)
+
+## Contributor:
+
+- [Gholakemohit](https://github.com/gholakemohit)
+
+## License & Copyright
+© [Arbaz Khan](https://arbazkhan4712.github.io/Contact.html)
+
+Licensed under the [MIT License](License)
+
+
---
+++ whats_app_ubuntu.py
@@ -0,0 +1,63 @@
+#import packages
+from selenium import webdriver
+from time import sleep
+
+#get the driver for individual browser
+driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+
+#scan the QR code
+print("Scan the QR code")
+driver.get("https://web.whatsapp.com/")
+
+#send message function
+def Sendmsg():
+
+    #enter name of receiver
+    name = str(raw_input("Enter the name of user or group : "))
+    #enter the message
+    msg = str(raw_input("Enter the message : "))
+    #enter the count
+    count = int(raw_input("Enter Number of count : "))
+
+    user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+    user.click()
+
+    msg_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+
+    for i in range(count):
+        msg_box.send_keys(msg)
+        button = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]')
+        button.click()
+
+#send image or video file function
+def sendimgvid():
+    #enter name of receiver
+    name = str(raw_input("Enter the name of user or group : "))
+    #enter file path
+    filepath = str(raw_input("Enter the file path (Image,Video) : "))
+
+    user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+    user.click()
+
+    attachment_box = driver.find_element_by_xpath('//div[@title = "Attach"]')
+    attachment_box.click()
+
+    imgvid_box = driver.find_element_by_xpath('//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
+    imgvid_box.send_keys(filepath)
+
+    sleep(3)
+
+    send_button = driver.find_element_by_xpath('//span[@data-icon="send-light"]')
+    send_button.click()
+
+#user input
+print("Press 1 for sending multiple messages \nPress 2 to send an image or video \nPress  to exit")
+n = int(input())
+if (n == 1):
+    Sendmsg()
+
+elif(n == 2):
+    sendimgvid()
+
+elif(n==3):
+    quit()
